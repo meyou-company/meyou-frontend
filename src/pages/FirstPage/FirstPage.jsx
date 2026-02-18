7e669ee3b6dbbd41bb8812127e459e0b4e9da17d
MariiaOtset <mariiaotset@gmail.com>
2026-02-18 19:05:57 +0000

    desktop adaptation done

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -28,7 +28,7 @@ export const FirstPage = () => {
       <div className="relative z-10 flex flex-col flex-1">
       {/* HEADER */}
       <header className="w-full border-gray-900 bg-purple-100">
-        <div className="mx-auto flex items-center justify-between px-1 pt-3 pb-5 md:py-[49px]">
+        <div className="mx-auto flex items-center justify-between px-1 pt-3 pb-5 md:pb-[49px] md:pt-[38px] xl:pb-8 xl:pt-0 xl:mx-10">
           {/* Left icons */}
           <div className="flex items-center gap-3">
             <button
@@ -50,12 +50,20 @@ export const FirstPage = () => {
           </div>
 
           {/* Logo */}
-          <div className={styles.logoText + " text-[#FF4FB1] font-extrabold font-[Montserrat] text-xl md:text-[40px] lg:text-6xl"}>
+          <div className={styles.logoText + " text-[#FF4FB1] font-extrabold font-[Montserrat] text-xl md:text-[40px] xl:text-6xl"}>
           ME YOU
           </div>
 
           {/* Right icons */}
           <div className="flex items-center gap-3">
+          <button
+          type="button"
+          onClick={() => navigate("/profile")}
+          aria-label="Profile"
+        >
+            <img src={profileIcons.balance} alt="" aria-hidden="true" className="hidden xl:flex h-12 mr-[60px]"/>
+        </button>
+
               <button
           type="button"
           className="menuBtn"
@@ -72,29 +80,30 @@ export const FirstPage = () => {
         >
             <img src={profileIcons.menu} alt="" aria-hidden="true" className="hidden md:block w-7 h-7 md:w-8 md:h-8 xl:w-12 xl:h-12"  />
         </button>
+
+        
             {/* <img src={profileIcons.search} alt="" aria-hidden="true" /> */}
-            {/* <div className="hidden xl:flex w-10 h-10 md:w-12 md:h-12 bg-gradient-to-bl from-pink-500 to-indigo-500" /> */}
           </div>
         </div>
       </header>
 
       {/* TABLET / DESKTOP NAV */}
       <section className="hidden md:block -ml-4 -mr-4 border-t-[0.1px] border-gray-900 bg-[#FCE9E9]">
-        <div className="mx-auto flex justify-between items-center px-[41px] py-[54px]">
+        <div className="mx-auto flex justify-between items-center px-[41px] py-[54px] xl:px-[60px] xl:py-10">
           <TabletNav />
         </div>
       </section>
     
       {/* STORIES */}
       <section className= "border-b-[0.1px] border-t-[0.1px] -ml-4 -mr-4 border-gray-900 bg-[#FCE9E9]">
-        <div className="mx-auto pl-4 pt-4 pr-1 md:pt-[23px] md:pb-[19px] md:pl-[38px]">
+        <div className="mx-auto pl-4 pt-4 pr-1 md:pt-[23px] md:pb-[19px] md:pl-[38px] xl:pl-[50px] xl:pt-4 xl:pb-[43px]">
           <div className="flex items-center justify-between mb-[4px] md:mb-6">
-            <h2 className="text-black font-[Montserrat] text-base md:text-xl pl-[9px] md:pl-0">
+            <h2 className="text-black font-[Montserrat] text-base md:text-xl pl-[9px] md:pl-0 xl:text-[28px]">
               Истории
             </h2>
           </div>
 
-          <div className={`flex gap-3 md:gap-[23px] lg:gap-[76px] overflow-x-auto pb-2 md:pb-0 snap-x snap-mandatory snap-center ${styles.scrollbarHide}`}>
+          <div className={`flex gap-3 md:gap-[23px] xl:gap-[76px] overflow-x-auto pb-2 pr-3 md:pb-0  xl:pl-4 snap-x snap-mandatory snap-center ${styles.scrollbarHide}`}>
     
             {/* Add story */}
             <StoryCircle type="add" />
@@ -102,15 +111,16 @@ export const FirstPage = () => {
             <StoryCircle status="online" />
             <StoryCircle status="offline" />
             <StoryCircle status="online" />
-            <StoryCircle type="add-more" />
-
+            <StoryCircle status="offline" />
+             <StoryCircle status="offline" />
+              <StoryCircle status="offline" />
           </div>
         </div>
       </section>
 
       {/* FEED */}
       <main className="flex-1">
-        <div className="max-w-6xl mx-auto my-[10px] md:my-5 px-[10px] md:px-5 space-y-[10px] md:space-y-5">
+        <div className="max-w-[1340px] mx-auto my-[10px] md:my-5 xl:my-[46px] px-[10px] md:px-5 space-y-[10px] md:space-y-5">
           <FeedCard
             name="Olivia Hugglton"
             time="3 days ago"
@@ -143,28 +153,28 @@ export const FirstPage = () => {
   <div className="flex justify-around items-center h-16 px-4">
     <button 
       onClick={() => navigate('/profile')}
-      className="flex flex-col items-center p-2 rounded-lg hover:bg-rose-200 active:bg-rose-300 transition-colors"
+      className="flex flex-col items-center p-2 rounded-xl hover:bg-rose-200 active:bg-rose-300 transition-colors"
     >
       <img src={homeIcon} alt="profile" className="w-[30px] h-[30px]" />
     </button>
     
     <button 
       onClick={() => navigate('/friends')}
-      className="flex flex-col items-center p-2 rounded-lg hover:bg-rose-200 active:bg-rose-300 transition-colors"
+      className="flex flex-col items-center p-2 rounded-xl hover:bg-rose-200 active:bg-rose-300 transition-colors"
     >
       <img src={userMenuIcon} alt="friends" className="w-[30px] h-[30px]" />
     </button>
     
     <button 
       onClick={() => navigate('/notifications')}
-      className="flex flex-col items-center p-2 rounded-lg hover:bg-rose-200 active:bg-rose-300 transition-colors"
+      className="flex flex-col items-center p-2 rounded-xl hover:bg-rose-200 active:bg-rose-300 transition-colors"
     >
       <img src={commentIcon} alt="notifications" className="w-[30px] h-[30px]" />
     </button>
     
     <button 
       onClick={() => navigate('/')}
-      className="flex flex-col items-center p-2 rounded-lg hover:bg-rose-200 active:bg-rose-300 transition-colors"
+      className="flex flex-col items-center p-2 rounded-xl hover:bg-rose-200 active:bg-rose-300 transition-colors"
     >
       <img src={profileIcons.menu} alt="messages" className="w-[30px] h-[30px]" />
     </button>
@@ -181,21 +191,21 @@ const StoryCircle = ({ status, type }) => {
   const isAdd = type === "add";
 
   return (
-    <button className="flex flex-col items-center gap-1 min-w-[70px]">
+    <button className="flex flex-col items-center gap-1">
       {/* Основной кружок */}
       {isAdd ? (
         <div className={styles.gradientBorder}>
-          <div className="relative flex items-center justify-center rounded-full w-16 h-16 md:w-[120px] md:h-[120px] bg-[#D5D5D5]">
+          <div className="relative flex items-center justify-center rounded-full w-16 h-16 md:w-[120px] md:h-[120px] xl:w-[160px] xl:h-[160px] bg-[#D5D5D5]">
             <img
               src={profileIcons.plus}
               alt="add story"
               aria-hidden="true"
-              className="w-8 h-8 md:w-12 md:h-12"
+              className="h-8 md:h-12 xl:h-20"
             />
           </div>
         </div>
       ) : (
-        <div className="relative flex items-center justify-center rounded-full border bg-[#D5D5D5] border-red-600 w-16 h-16 md:w-[120px] md:h-[120px]">
+        <div className="relative flex items-center justify-center rounded-full border bg-[#D5D5D5] border-[#FF0B0B] w-16 h-16 md:w-[120px] md:h-[120px] xl:w-[160px] xl:h-[160px] xl:border-[3px]">
           <img
             src={userIcon}
             alt="user story"
@@ -205,7 +215,7 @@ const StoryCircle = ({ status, type }) => {
 
           {status && (
             <span
-              className={`absolute right-[2px] top-[2px] w-2.5 h-2.5 md:w-3 md:h-3 md:top-3 md:right-[7px] rounded-full ${
+              className={`absolute right-[2px] top-[2px] w-2.5 h-2.5 md:w-3 md:h-3 xl:w-5 xl:h-5 md:top-3 md:right-[7px] rounded-full ${
                 status === "online"
                   ? "bg-green-700"
                   : "bg-zinc-300 border border-black/40"
@@ -216,7 +226,7 @@ const StoryCircle = ({ status, type }) => {
       )}
 
       {/* Подпись */}
-      <span className="text-[8px] md:text-xs font-[Montserrat] text-black underline">
+      <span className="text-[8px] md:text-xs xl:text-xl font-[Montserrat] text-black underline">
         {isAdd ? "добавить" : status === "online" ? "online" : "story"}
       </span>
     </button>
@@ -226,7 +236,7 @@ const StoryCircle = ({ status, type }) => {
 
 const FeedCard = ({ name, time, location, status, text }) => {
   return (
-    <article className="bg-slate-50 shadow-sm px-[6px] pt-[6px] pb-[11px] md:p-[10px] space-y-3 relative">
+    <article className="bg-slate-50 shadow-sm px-[6px] pt-[6px] pb-[11px] md:p-[10px] xl:!mb-[29px] space-y-3 relative">
       {/* Header */}
       <div className="flex justify-between items-start">
         <div className="flex gap-[7px]">
@@ -234,7 +244,7 @@ const FeedCard = ({ name, time, location, status, text }) => {
             <img
               src={userIcon}
               alt={name}
-              className="w-10 h-10 md:w-[60px] md:h-[60px] rounded-full object-none bg-gray-300"
+              className=" h-10  md:h-[60px] xl:h-20 rounded-full object-none bg-gray-300"
             />
             <span
               className={`absolute right-[2px] top-[3px] w-[6px] h-[6px] md:w-2 md:h-2 md:top-[7px] md:right-[3px] rounded-full ${
@@ -246,10 +256,10 @@ const FeedCard = ({ name, time, location, status, text }) => {
           </div>
           <div className="flex flex-col mt-[5px] gap-[3px]">
             
-            <span className="text-[8px] md:text-xs text-black font-[Montserrat] underline">
+            <span className="text-[8px] md:text-xs xl:text-xl text-black font-[Montserrat] underline">
               {time}
             </span>
-            <span className="text-xs md:text-sm font-[Montserrat] underline bg-gradient-to-r from-[#FF4FB1] to-[#4F6BFF] bg-clip-text text-transparent">
+            <span className="text-xs md:text-sm xl:text-xl font-[Montserrat] underline bg-gradient-to-r from-[#FF4FB1] to-[#4F6BFF] bg-clip-text text-transparent">
              {name}
             </span>
 
@@ -257,28 +267,28 @@ const FeedCard = ({ name, time, location, status, text }) => {
         </div>
 
     <div>
-        <div className="flex items-center mt-[13px] mr-[19px]">
+        <div className="flex items-center mt-[13px] mr-[19px] xl:hidden">
           <img src={locationIcon} alt="location" className="w-[4px] h-[5px] mr-1 mt-[1px] md:w-[10px] md:h-[13px]" />
           <span className="relative text-[10px] md:text-xs text-pink-500 font-[Montserrat] mr-[7px] inline-block">
           {location}
           <span className="absolute bottom-[2px] left-0 right-0 h-[0.5px] bg-pink-500"></span>
           </span>
         </div>
-         <img src={closeIcon} alt="close" className="absolute top-[13px] right-[14px] w-[6px] h-[6px] md:w-2 md:h-2" />
+         <img src={closeIcon} alt="close" className="absolute top-[13px] xl:top-7 right-[14px] xl:right-7 h-[6px] md:h-2 xl:h-3" />
        </div>
     </div>
 
       {/* Text */}
-      <p className="text-xs text-gray-900 font-[Montserrat] font-medium md:font-normal underline">
+      <p className="text-xs text-gray-900 font-[Montserrat] font-medium md:font-normal xl:text-xl underline">
         {text}
       </p>
 
       {/* Image placeholder */}
-      <div className="!mt-[19px] md:!mt-[10px] h-[202px] md:h-40 bg-black/5" />
+      <div className="!mt-[19px] md:!mt-[10px] h-[202px] md:h-40 xl:h-[290px] bg-black/5" />
 
       {/* Actions */}
-      <div className="flex justify-center mt-3">
-        <div className="flex gap-[41px] md:gap-[60px]">
+      <div className="flex justify-center mt-3 xl:!mt-[52px] xl:!mb-[38px]">
+        <div className="flex gap-[41px] md:gap-[60px] xl:gap-36">
           <ActionIcon icon={likeIcon} label="125" />
           <ActionIcon icon={commentsIcon} label="256" /> 
           <ActionIcon icon={savedIcon} label="21" />
@@ -295,9 +305,9 @@ const ActionIcon = ({ icon, label }) => (
     <img 
       src={icon} 
       alt={label} 
-      className="w-6 h-6 md:w-9 md:h-9" 
+      className="h-6 md:h-9 xl:h-11" 
     />
-    <span className="text-black text-[8px] md:text-xs font-normal font-['Montserrat']">{label}</span>
+    <span className="text-black text-[8px] md:text-xs xl:text-xl font-normal xl:font-bold font-['Montserrat']">{label}</span>
   </button>
 );
 
@@ -338,7 +348,7 @@ const ActionIcon = ({ icon, label }) => (
 //           <button
 //             key={index}
 //             onClick={() => navigate(item.route)}
-//             className="flex flex-col items-center p-2 rounded-lg hover:bg-rose-200 active:bg-rose-300 transition-colors flex-1 h-full"
+//             className="flex flex-col items-center p-2 rounded-xl hover:bg-rose-200 active:bg-rose-300 transition-colors flex-1 h-full"
 //             aria-label={item.label}
 //           >
 //             <img 
@@ -356,29 +366,75 @@ const ActionIcon = ({ icon, label }) => (
 const TabletNav = () => {
   const navigate = useNavigate();
 
-  const navItems = [
-    { icon: homeIcon, route: "/profile", label: "Profile" },
-    { icon: profileIcons.video, route: "/", label: "" },
-    { icon: profileIcons.user, route: "/friends", label: "Friends" },
-    { icon: commentIcon, route: "/notifications", label: "Notifications" },
-  ];
-
   return (
     <nav className="w-full">
-      <div className="flex justify-between gap-12 lg:gap-20">
-        {navItems.map((item, index) => (
-          <button
-            key={index}
-            onClick={() => navigate(item.route)}
-            className="flex flex-col items-center gap-2 group"
-          >
-            <img
-              src={item.icon}
-              alt={item.label}
-              className=" h-8 lg:w-10 lg:h-10 group-hover:scale-110 transition"
-            />
-          </button>
-        ))}
+      <div className="flex justify-between gap-12 xl:gap-20">
+
+        {/* 1 кнопка */}
+        <button
+          onClick={() => navigate("/profile")}
+          className="flex flex-col items-center gap-2 group"
+        >
+          {/* md = home */}
+          <img
+            src={homeIcon}
+            alt="Profile"
+            className="h-8 xl:hidden"
+          />
+
+          {/* xl = plus */}
+          <img
+            src={profileIcons.plus}
+            alt="Add"
+            className="hidden xl:block h-8 xl:h-[50px]"
+          />
+        </button>
+
+
+        {/* 2 кнопка */}
+        <button
+          onClick={() => navigate("/")}
+          className="flex flex-col items-center gap-2 group"
+        >
+          {/* md = plus */}
+          <img
+            src={profileIcons.plus}
+            alt="Add"
+            className="h-8 xl:hidden"
+          />
+
+          {/* xl = video (раскомментированная) */}
+          <img
+            src={profileIcons.video}
+            alt="Video"
+            className="hidden xl:block h-8 xl:h-[50px]"
+          />
+        </button>
+
+
+        {/* Остальные без изменений */}
+        <button
+          onClick={() => navigate("/friends")}
+          className="flex flex-col items-center gap-2 group"
+        >
+          <img
+            src={profileIcons.user}
+            alt="Friends"
+            className="h-8 xl:h-[50px] group-hover:scale-110 transition"
+          />
+        </button>
+
+        <button
+          onClick={() => navigate("/notifications")}
+          className="flex flex-col items-center gap-2 group"
+        >
+          <img
+            src={commentIcon}
+            alt="Notifications"
+            className="h-8 xl:h-[50px] group-hover:scale-110 transition"
+          />
+        </button>
+
       </div>
     </nav>
   );
