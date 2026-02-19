06a0ac8faf5c4dfcc37b075bdaa40a544cbf160b
MariiaOtset <mariiaotset@gmail.com>
2026-02-19 19:10:18 +0000

    refactoring part 3

--- src/components/FirstPage/FirstPageView.jsx
+++ src/components/FirstPage/FirstPageView.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import profileIcons from "../../constants/profileIcons";
 import "./FirstPageView.scss";
 
@@ -9,6 +10,7 @@ export default function FirstPageView({
   onGoNotifications,
   onGoHome,
 }) {
+
   return (
     <div className="relative min-h-screen flex flex-col pb-10 md:pb-0">
       {/* background */}
@@ -54,20 +56,22 @@ export default function FirstPageView({
           </div>
         </header>
 
-      //!  {/* TABLET / DESKTOP NAV */}
+        {/* TABLET / DESKTOP NAV  */}
       <section className="hidden md:block -ml-4 -mr-4 border-t-[0.1px] border-gray-900 bg-[#FCE9E9]">
-        <div className="mx-auto flex justify-between items-center px-[41px] py-[54px] xl:px-[60px] xl:py-10">
+        <div className="mx-auto flex justify-between items-center px-[41px] py-10 xl:py-10 xl:px-[60px]">
+
           <TabletNav />
+
         </div>
       </section>
 
         {/* STORIES */}
         <section className= "border-b-[0.1px] border-t-[0.1px] -ml-4 -mr-4 border-gray-900 bg-[#FCE9E9]">
-          <div className="mx-auto pl-4 pt-4 pr-1 md:pt-[23px] md:pb-[19px] md:pl-[38px] xl:pl-[50px] xl:pt-4 xl:pb-[43px]">
+          <div className="mx-auto pl-4 pt-4 pr-1 md:pt-[23px] md:pb-[19px] md:pl-[38px] xl:pl-[50px] xl:pt-4 xl:pb-[13px]">
             <h2 className="mb-1 md:mb-6 text-black font-[Montserrat] text-base md:text-xl pl-[9px] md:pl-0 xl:text-[28px]">Истории</h2>
 
             <div
-              className="flex gap-3 md:gap-[23px] xl:gap-[76px] overflow-x-auto pb-2 pr-3 md:pb-0  xl:pl-4 snap-x snap-mandatory snap-center scrollbarHide"
+              className="flex gap-3 md:gap-[23px] xl:gap-10 overflow-x-auto pb-2 pr-3 md:pb-0  xl:pl-4 snap-x snap-mandatory snap-center scrollbarHide"
             >
               <StoryCircle type="add" />
               <StoryCircle status="online" />
@@ -133,15 +137,15 @@ function StoryCircle({ status, type }) {
     <button className="flex flex-col items-center gap-1">
       {isAdd ? (
         <div className="gradientBorder">
-          <div className="relative flex items-center justify-center rounded-full w-14 h-14 md:w-[100px] md:h-[100px] xl:w-[120px] xl:h-[120px] bg-[#D5D5D5]">
+          <div className="relative flex items-center justify-center rounded-full w-14 h-14 md:w-[97px] md:h-[97px] bg-[#D5D5D5]">
             <img src={profileIcons.plus}  alt="add story"
                  aria-hidden="true"
-                 className="h-8 md:h-12 xl:h-20" />
+                 className="h-8 md:h-12" />
           </div>
         </div>
       ) : (
-        <div className="relative rounded-full w-14 h-14 border bg-[#D5D5D5] border-[#FF0B0B] flex items-center justify-center md:w-[100px] md:h-[100px] xl:w-[120px] xl:h-[120px] xl:border-[3px]">
-          <img src={profileIcons.userStory} alt="user story"  className="w-[26px] h-[26px] md:w-12 md:h-12"/>
+        <div className="relative rounded-full w-14 h-14 border bg-[#D5D5D5] border-[#FF0B0B] flex items-center justify-center md:w-[100px] md:h-[100px] xl:border-[3px]">
+          <img src={profileIcons.userStory} alt="user story" className="w-[26px] h-[26px] md:w-12 md:h-12"/>
 
           {status && (
             <span
@@ -159,6 +163,53 @@ function StoryCircle({ status, type }) {
     </button>
   );
 }
+
+
+// / const StoryCircle = ({ status, type }) => {
+//   const isAdd = type === "add";
+
+//   return (
+//     <button className="flex flex-col items-center gap-1">
+//       {/* Основной кружок */}
+//       {isAdd ? (
+//         <div className={styles.gradientBorder}>
+//           <div className="relative flex items-center justify-center rounded-full w-16 h-16 md:w-[120px] md:h-[120px] xl:w-[160px] xl:h-[160px] bg-[#D5D5D5]">
+//             <img
+//               src={profileIcons.plus}
+//               alt="add story"
+//               aria-hidden="true"
+//               className="h-8 md:h-12 xl:h-20"
+//             />
+//           </div>
+//         </div>
+//       ) : (
+//         <div className="relative flex items-center justify-center rounded-full border bg-[#D5D5D5] border-[#FF0B0B] w-16 h-16 md:w-[120px] md:h-[120px] xl:w-[160px] xl:h-[160px] xl:border-[3px]">
+//           <img
+//             src={userIcon}
+//             alt="user story"
+//             aria-hidden="true"
+//             className="w-[26px] h-[26px] md:w-12 md:h-12"
+//           />
+
+          // {status && (
+          //   <span
+          //     className={`absolute right-[2px] top-[2px] w-2.5 h-2.5 md:w-3 md:h-3 xl:w-5 xl:h-5 md:top-3 md:right-[7px] rounded-full ${
+          //       status === "online"
+          //         ? "bg-green-700"
+          //         : "bg-zinc-300 border border-black/40"
+          //     }`}
+          //   />
+          // )}
+//         </div>
+//       )}
+
+//       {/* Подпись */}
+//       <span className="text-[8px] md:text-xs xl:text-xl font-[Montserrat] text-black underline">
+//         {isAdd ? "добавить" : status === "online" ? "online" : "story"}
+//       </span>
+//     </button>
+//   );
+// };
 
 const FeedCard = ({ name, time, location, status, text }) => {
   return (
@@ -210,7 +261,7 @@ const FeedCard = ({ name, time, location, status, text }) => {
       </p>
 
       {/* Image placeholder */}
-      <div className="!mt-[19px] md:!mt-[10px] h-[202px] md:h-40 xl:h-[290px] bg-black/5" />
+      <div className="!mt-[19px] md:!mt-[10px] h-80 bg-black/5" />
 
       {/* Actions */}
       <div className="flex justify-center mt-3 xl:!mt-[52px] xl:!mb-[38px]">
@@ -239,69 +290,69 @@ function ActionIcon({ icon, label }) {
   );
 }
 
-// const TabletNav = () => {
-//   const navigate = useNavigate();
+const TabletNav = () => {
+  const navigate = useNavigate();
 
-//   return (
-//     <nav className="w-full">
-//       <div className="flex justify-between gap-12 xl:gap-20">
+  return (
+    <nav className="w-full">
+      <div className="flex justify-between gap-12 xl:gap-20">
 
-//         {/* 1 кнопка */}
-//         <button
-//           onClick={() => navigate("/profile")}
-//           className="flex flex-col items-center gap-2 group"
-//         >
-//           {/* md = home */}
-//           <img
-//             src={homeIcon}
-//             alt="Profile"
-//             className="h-8 xl:hidden"
-//           />
+        {/* 1 кнопка */}
+        <button
+          onClick={() => navigate("/profile")}
+          className="flex flex-col items-center gap-2 group"
+        >
+          {/* md = home */}
+          <img
+            src={profileIcons.home}
+            alt="Profile"
+            className="h-8 xl:hidden"
+          />
 
-//           {/* xl = plus */}
-//           <img
-//             src={profileIcons.plus}
-//             alt="Add"
-//             className="hidden xl:block h-8 xl:h-[50px]"
-//           />
-//         </button>
+          {/* xl = plus */}
+          <img
+            src={profileIcons.plus}
+            alt="Add"
+            className="hidden xl:block h-8 xl:h-[50px]"
+          />
+        </button>
 
-//        {/* Остальные без изменений */}
+       {/* Остальные без изменений */}
   
-//         <button
-//           onClick={() => navigate("/")}
-//           className="flex flex-col items-center gap-2 group"
-//         >
-//           <img
-//             src={profileIcons.video}
-//             alt="Video"
-//             className="hidden md:block h-8 xl:h-[50px]"
-//           />
-//         </button>
+        <button
+          onClick={() => navigate("/")}
+          className="flex flex-col items-center gap-2 group"
+        >
+          <img
+            src={profileIcons.video}
+            alt="Video"
+            className="hidden md:block h-8 xl:h-[50px]"
+          />
+        </button>
  
-//         <button
-//           onClick={() => navigate("/friends")}
-//           className="flex flex-col items-center gap-2 group"
-//         >
-//           <img
-//             src={profileIcons.user}
-//             alt="Friends"
-//             className="h-8 xl:h-[50px] group-hover:scale-110 transition"
-//           />
-//         </button>
+        <button
+          onClick={() => navigate("/friends")}
+          className="flex flex-col items-center gap-2 group"
+        >
+          <img
+            src={profileIcons.friends}
+            alt="Friends"
+            className="h-8 xl:h-[50px]"
+          />
+        </button>
 
-//         <button
-//           onClick={() => navigate("/notifications")}
-//           className="flex flex-col items-center gap-2 group"
-//         >
-//           <img
-//             src={commentIcon}
-//             alt="Notifications"
-//             className="h-8 xl:h-[50px] group-hover:scale-110 transition"
-//           />
-//         </button>
+        <button
+          onClick={() => navigate("/notifications")}
+          className="flex flex-col items-center gap-2 group"
+        >
+          <img
+            src={profileIcons.bell}
+            alt="Notifications"
+            className="h-8 xl:h-[50px]"
+          />
+        </button>
 
-//       </div>
-//     </nav>
-//   );
-// };
+      </div>
+    </nav>
+  );
+};
