2ee4ec6c3f5e64bdc537ee6853b8cb42d7d306f9
kuzma939 <meyou.company9339@gmail.com>
2026-02-19 09:32:24 +0100

    icon

--- src/constants/profileIcons.js
+++ src/constants/profileIcons.js
@@ -1,23 +1,52 @@
 /** Шляхи до іконок з public/home/ */
+
 const HOME = "/home";
+const ICON1 = "/icon1";
+const BLACK = "/icon-black";
 
 const profileIcons = {
-  msg: `${HOME}/my-massages.svg`,
-  messages: `${HOME}/my-massages.svg`,
-  plus: `${HOME}/gg_add.svg`,
+  /* === HOME === */
+  home: `${HOME}/home.svg`,
+  search: `${HOME}/poisk.svg`,
   video: `${HOME}/video-two.svg`,
   live: `${HOME}/camera.svg`,
   posts: `${HOME}/camera.svg`,
-  search: `${HOME}/poisk.svg`,
   balance: `${HOME}/balance.svg`,
   menu: `${HOME}/menu.svg`,
-  home: `${HOME}/home.svg`,
   user: `${HOME}/user1.svg`,
   friends: `${HOME}/user-friends.svg`,
   saved: `${HOME}/saved.svg`,
-  bell: `${HOME}/notifications.svg`,
+  like: `${HOME}/like.svg`,
+  comments: `${HOME}/comments.svg`,
+  location: `${HOME}/location.svg`,
+  share: `${HOME}/to-share.svg`,
   notifications: `${HOME}/notifications.svg`,
-  chat: `${HOME}/my-massages.svg`,
+  bell: `${HOME}/notifications.svg`,
   vip: `${HOME}/vip-fill.svg`,
+  plus: `${HOME}/gg_add.svg`,
+  chat: `${HOME}/my-massages.svg`,
+  /* ========= ICON1 ========= */
+  userStory: `${ICON1}/user.svg`,
+  close: `${ICON1}/close.svg`,
+  savedPost: `${ICON1}/saved.svg`,
+  sms: `${ICON1}/sms.svg`,
+  comment: `${ICON1}/comment.svg`,
+  userMenu: `${ICON1}/userMenu.svg`,
+  password: `${ICON1}/password.svg`,
+  eye: `${ICON1}/oko.svg`,
+  eyeOff: `${ICON1}/oko-off.svg`,
+
+  /* ========= BLACK (actions, buttons) ========= */
+  plusBlack: `${BLACK}/plus.svg`,
+  videoBlack: `${BLACK}/videocamera.svg`,
+  liveBlack: `${BLACK}/Group.svg`,
+  pencilBlack: `${BLACK}/pencil.svg`,
+  layoutBlack: `${BLACK}/layout-outline.svg`,
+  bellBlack: `${BLACK}/line-md_bell.svg`,
+  listBlack: `${BLACK}/gg_list.svg`,
+  closeBlack: `${BLACK}/cross-filled.svg`,
+  lockBlack: `${BLACK}/lock.svg`,
+  vipButton: `${BLACK}/vip-button.svg`,
 };
+
 export default profileIcons;
