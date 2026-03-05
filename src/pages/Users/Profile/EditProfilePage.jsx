07a416dff5d244fdba24ee6819035a8587e3b9c5
Lesia Sasiuk <chumachocl@gmail.com>
2026-03-05 12:37:08 +0200

    Created profile edit logic

--- src/pages/Users/Profile/EditProfilePage.jsx
+++ src/pages/Users/Profile/EditProfilePage.jsx
@@ -1,7 +1,25 @@
+import { toast } from 'sonner'
+import { useNavigate } from 'react-router-dom'
+
+import EditProfileForm from '../../../components/Users/Profile/EditProfileForm/EditProfileForm'
+import { profileApi } from '../../../services/profileApi'
+import { useAuthStore } from '../../../zustand/useAuthStore'
+
 export default function EditProfilePage() {
+  const navigate = useNavigate()
+
+  const handleSave = async (payload) => {
+    try {
+      await profileApi.updateProfile(payload)
+
+      toast.success('Профіль успішно оновлено')
+      await useAuthStore.getState().refreshMe()
+    } catch (error) {
+      console.error('Помилка оновлення профілю:', error)
+    }
+  }
+
   return (
-    <div style={{ padding: 16 }}>
-      <h2>Редагування профілю (в роботі)</h2>
-    </div>
-  );
+    <EditProfileForm onSave={handleSave} onBack={() => navigate('/profile')} />
+  )
 }
