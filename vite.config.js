e6ab861bb0f4f99fe2d9fed986d5d806ba1ac995
CodeByArtem <150847319+CodeByArtem@users.noreply.github.com>
2026-04-06 22:17:04 +0200

    versal

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173
-  }
-})
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://meyou-backend-0xsz.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
