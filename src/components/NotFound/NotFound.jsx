c19a34d22909b21e3abc863647f89d1703331ba1
MariiaOtset <info@alongo.co.uk>
2026-04-29 19:55:36 +0100

    Reduced the element size and desktop on the gift page + removed the indent if there is no gift. base of NotFoundPage created

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import "./NotFound.scss";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="NotFound_title">Ой, что-то пошло не так...</h1>
+      <div>NotFound</div>
+    </div>
+  )
+}
+
+
+
